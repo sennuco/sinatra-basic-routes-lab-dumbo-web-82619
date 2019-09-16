d47320f8fb6596f3aa051b5abf6d66d1218acf36
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 21:15:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Mei"
+  end
+  
+  get '/hometown' do
+     "My hometown is Brooklyn, New York"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Bare in the Basin by Gaelic Storm "
+  end 
+  
 end
